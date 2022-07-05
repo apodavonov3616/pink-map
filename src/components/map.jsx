db927a8e8e081d54339a1da20bd33148eb55c2aa
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-07-05 14:08:20 -0400

    got specific places to show

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,4 +1,5 @@
-import { useState, useMemo, userCallback, useRef } from "react";
+/*global google*/
+import React, { useState, useMemo, userCallback, useRef } from "react";
 import {
     GoogleMap,
     Marker,
@@ -7,22 +8,60 @@ import {
     MarkerClusterer
 } from "@react-google-maps/api"
 import mapStyles from "./../mapStyles"
+import Search from "./search";
 
 export default function Map() {
 
     const center = useMemo(() => ({ lat: 40.73629, lng: -73.99379 }), []);
     const options = {
-        styles: mapStyles
+        // styles: mapStyles,
+        disableDefaultUI: true,
+        zoomControl: true
     }
+
+    const mapRef = React.useRef();
+  const onMapLoad = React.useCallback(map => { 
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(12);
+    let map = mapRef.current;
+
+
+
+    let request = { 
+      location: { lat, lng },
+      radius: "500",
+      type: ["church"]  
+    };
+
+    let service = new google.maps.places.PlacesService(mapRef.current);
+    service.nearbySearch(request, callback);
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 0; i < results.length; i++) {
+          let place = results[i];
+          new google.maps.Marker({
+            position: place.geometry.location,
+            map
+          });
+        }
+      }
+    }
+  }, []);
 
     return <div className="container">
         <div className="controls"><h1>Map</h1></div>
+        <Search panTo={panTo} />
         <div className="map">
             <GoogleMap 
             zoom={10} 
             center={center} 
             mapContainerClassName="map-container"
             options={options}
+            onLoad={onMapLoad}
             >
             </GoogleMap>
         </div>
